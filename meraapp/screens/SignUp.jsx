8092e5732c6c18284f52899c9c2fd224da206043
Shubham Kumar <shubhamsit31@gmail.com>
2024-07-13 14:01:41 +0530

    commit

--- meraapp/screens/SignUp.jsx
+++ meraapp/screens/SignUp.jsx
@@ -1,111 +1,111 @@
-import { StyleSheet, Text, View,Image, TextInput,Button } from 'react-native'
+import { StyleSheet, Text, View, Image, TextInput, Button } from 'react-native'
 import React from 'react'
 
-const Login = ({onLoginPress}) => {
-  return (
-    <View style={styles.view1}>
+const Login = ({ onLoginPress }) => {
+    return (
+        <View style={styles.view1}>
 
-        <View style={styles.view2}>
+            <View style={styles.view2}>
 
-            <View style={styles.imageContainer}>
-                 <Image  style={styles.image}source={{uri:'https://img.freepik.com/premium-vector/profile-security-flat-style-design-vector-illustration-stock-illustration_357500-781.jpg?w=900'} }
-                 
-                 />
-            
+                <View style={styles.imageContainer}>
+                    <Image style={styles.image} source={{ uri: 'https://img.freepik.com/premium-vector/profile-security-flat-style-design-vector-illustration-stock-illustration_357500-781.jpg?w=900' }}
+
+                    />
+
+                </View>
+
+                <View>
+
+                    <TextInput
+                        placeholder='Username'
+                        keyboardType="default"
+                        style={styles.inputBox}
+
+                    />
+
+                    <TextInput
+                        placeholder='Email'
+                        keyboardType="email-address"
+                        style={styles.inputBox}
+
+                    />
+
+                    <TextInput
+                        placeholder='Password'
+                        secureTextEntry={true}
+                        style={styles.inputBox}
+
+                    />
+
+
+
+                    <View style={styles.btnContainer}>
+                        <Button
+                            title='Signup'
+                            style={styles.button}
+
+                        />
+                    </View>
+                    <View style={styles.btnContainer}>
+                        <Button
+                            title='Login'
+                            color='orange'
+                            onPress={onLoginPress}
+                        />
+
+
+                        <View />
+
+
+
+                    </View>
+
+
+
+
+                </View>
+
+
+
             </View>
-
-            <View>
-
-            <TextInput
-                placeholder='Username'
-                keyboardType="default" 
-                style={styles.inputBox}
-                
-                />
-
-                <TextInput
-                placeholder='Email'
-                keyboardType="email-address" 
-                style={styles.inputBox}
-                
-                />
-
-                 <TextInput
-                placeholder='Password'
-                secureTextEntry={true} 
-                style={styles.inputBox}
-                
-                />
-
-
-
-<View style={styles.btnContainer}>
-                <Button
-                title='Signup'
-                style={styles.button}
-                
-                />
-</View>
-<View style={styles.btnContainer}>
-                <Button
-                title='Login'
-                color='orange'
-                onPress={onLoginPress}
-                />
-
-
-                <View/>
-
-                
-
-</View>
-               
-
-
-
-            </View>
-
 
 
         </View>
-
-     
-    </View>
-  )
+    )
 }
 
 export default Login
 
 const styles = StyleSheet.create({
 
-    view1:{
-        backgroundColor:'white',
-        width:'100%',
-        minHeight:'100%',
-        flex:1,
-        alignItems:'center',
-        justifyContent:'center',
+    view1: {
+        backgroundColor: 'white',
+        width: '100%',
+        minHeight: '100%',
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
 
     },
-    view2:{
-        backgroundColor:'white',
-        width:'80%',
-        minHeight:'80%',
+    view2: {
+        backgroundColor: 'white',
+        width: '80%',
+        minHeight: '80%',
 
     },
-    image:{
-        width:'98%',
-        height:'50%',
+    image: {
+        width: '98%',
+        height: '50%',
         // position:'absolute',
-        top:100
-        
-     
+        top: 100
+
+
     },
-    imageContainer:{
+    imageContainer: {
         // backgroundColor:'blue'
 
     },
-    inputBox:{
+    inputBox: {
         height: 40,
         borderColor: 'gray',
         borderWidth: 1,
@@ -114,18 +114,18 @@ const styles = StyleSheet.create({
         borderRadius: 5,
 
     },
-    btnContainer:{
-    
+    btnContainer: {
+
         paddingVertical: 10,
         paddingHorizontal: 20,
         borderRadius: 5,
-      
-        marginTop:20,
 
-    
+        marginTop: 20,
+
+
     },
-    btn:{
-        marginBottom:5,
+    btn: {
+        marginBottom: 5,
 
     }
 })
